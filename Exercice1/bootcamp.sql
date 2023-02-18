d6ffbc6339c5e8b297b33552963bc2c938a4924d
yanbert <yanbert@gmail.com>
2023-02-18 17:12:47 +0000

    Mes commentaires dans le code
    JE ne vois pas ton exercice 
    tout est commenté

--- Exercice1/bootcamp.sql
+++ Exercice1/bootcamp.sql
@@ -1,3 +1,6 @@
+JE ne vois pas ton exercice 
+tout est commenté
+
 ---CREATE DATABASE bootcamp;
 
 ---CREATE TABLE students(id SERIAL PRIMARY KEY, last_name VARCHAR(50) NOT NULL, first_name VARCHAR(100) NOT NULL, birth_date DATE NOT NULL);
